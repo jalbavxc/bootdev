49ffe6f6bc02c937439c27928825e7eb30d53a46
Victor Jalba <jalbav.xc@gmail.com>
2025-04-26 21:35:39 -0700

    V3:2 Asteroids collision

---
+++ player.py
@@ -0,0 +1,47 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "green", self.triangle(), 0)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def update(self, dt):
+        super().update(dt)
+
+    # Move the player (you might already have this)
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    # Clamp the player position to the screen boundaries
+        self.position.x = max(0, min(self.position.x, SCREEN_WIDTH))
+        self.position.y = max(0, min(self.position.y, SCREEN_HEIGHT))
+
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
